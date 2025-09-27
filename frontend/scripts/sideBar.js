40099ef9c80ad19617f2d8bcf000b4110ef3f97a
Hiagoo26 <hiago2608@gmail.com>
2025-09-27 17:52:00 -0300

    style: sideBar, overlay, refactor: sideBar.js e algumas mudanças nos arquivos html para ficarem padronizados

--- frontend/scripts/sideBar.js
+++ frontend/scripts/sideBar.js
@@ -2,22 +2,41 @@ const menuBtn = document.querySelector('.menu-button');
 const sideBar = document.getElementById('sideBar')
 const overlay = document.getElementById('overlay')
 const fecharSideBar = document.getElementById('fecharSideBar');
+const btnLogin = document.getElementById('btnLogin');
+const btnCadastro = document.getElementById('btnCadastro');
+
+function fechaSideBar() {
+    sideBar.classList.remove("mostra");
+    overlay.classList.add("hidden");
+}
+
+function abrirSideBar() {
+    sideBar.classList.add("mostra");
+    overlay.classList.remove("hidden");
+}
 
 menuBtn.removeAttribute("disabled");
 
 menuBtn.addEventListener("click", () => {
-    overlay.classList.remove("hidden");
-    sideBar.classList.add("mostra");
+    abrirSideBar();
 });
 
 fecharSideBar.addEventListener("click", () => {
-    overlay.classList.add("hidden");
-    sideBar.classList.remove("mostra");
+    fechaSideBar();
 });
 
 overlay.addEventListener("click", (e) => {
     if(e.target === overlay) {
-        overlay.classList.add("hidden");
-        sideBar.classList.remove("mostra");
+        fechaSideBar();
     }
 })
+
+btnLogin.addEventListener("click", () => {
+    fechaSideBar();
+    abrirLogin();
+});
+
+btnCadastro.addEventListener("click", () => {
+    fechaSideBar();
+    abrirCadastro();
+});
